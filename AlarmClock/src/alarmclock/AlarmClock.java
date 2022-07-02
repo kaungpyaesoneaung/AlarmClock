81d0df443e8b95d212c9a19a48d0ee9abb1859b4
kaungpyaesoneaung <kaungpyae.kpp16@gmail.com>
2022-07-02 15:21:42 +0630

    Alarm Clock
    Messy code Java with Jar File

---
+++ AlarmClock/src/alarmclock/AlarmClock.java
@@ -0,0 +1,15 @@
+
+package alarmclock;
+
+
+public class AlarmClock {
+
+
+    public static void main(String[] args) {
+      
+        Clock1 frame = new Clock1();
+
+        frame.setVisible(true);
+    }
+    
+}
